1e64d355c5e35fa6ffebd2e144721d9534fbd09c
styletab <katherinetab@gmail.com>
2016-09-14 16:30:00 -0400

    Answers to People Lab
    Answers to questions in both Ruby and Javascript

--- bin/people-array.js
+++ bin/people-array.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// let loadPeople = require('../lib/people.js');
+ let loadPeople = require('../lib/people.js');
 //
 // loadPeople().then((people) => {
 //   let count = people.reduce((m /*, p */) => m + 1, 0);
@@ -16,8 +16,81 @@ loadPeople().then((people) => {
 const increment = (accumulator) => {
   return accumulator + 1;
 };
-  let count = people.reduct(increment, 0);
 
-  console.log('total ppl (using reduce): ', count);
+const isFemale = (person) => {
+  return person.gender === 'f';
+};
 
-});
+ let femaleCount = people.filter(isFemale).reduce(increment, 0);
+
+ console.log('total females (using reduce): ', femaleCount);
+
+const isMale = (person) => {
+  return person.gender === 'm';
+};
+
+  let maleCount = people.filter(isMale).reduce(increment, 0);
+  //people.filter(!isFemale).reduce(increment, 0); // WRONG
+  // this doesn't answer the question How many males? it answers how many not
+  // females?
+  // also syntax error
+
+  console.log('total males (using reduce): ', maleCount);
+
+// Count all the people who are older than you (or just pick an age).
+
+const isOlder = (person) => {
+  return person.age() > 25;
+};
+
+let countOfOlder = people.filter(isOlder).reduce(increment, 0);
+
+console.log('Total ppl over 25 (using reduce) ', countOfOlder);
+
+
+
+ // Count all the people who are younger than you (or just pick an age).
+
+ const isYounger = (person) => {
+   return person.age() <= 25;
+ };
+
+ let countOfYounger = people.filter(isYounger).reduce(increment, 0);
+
+ console.log('Total ppl under 25 (using reduce) ', countOfYounger);
+
+
+ // Count all the people whose first name and last name start with the
+ // same letter.
+
+const sameLetter = (person) => {
+  return person.given_name[0] === person.surname[0];
+};
+
+let sameName = people.filter(sameLetter).reduce(increment, 0);
+
+console.log('Total ppl with same letter name ', sameName);
+
+// Calculate the average age of all the people.
+
+const ageToday = (person) => {
+   return person.age();
+};
+
+const sumAge = (previous, currentPerson) => {
+  return previous + currentPerson.age();
+};
+
+let getAvgAge = people.filter(ageToday).reduce(sumAge, 0) / people.length;
+
+console.log('Avg age is ', getAvgAge);
+
+
+
+
+
+
+
+
+
+  }).catch((error) => console.log(error));
--- bin/people_array.rb
+++ bin/people_array.rb
@@ -2,4 +2,37 @@ require_relative '../lib/people.rb'
 
 people = People.new.people
 
-p people.count
+puts "Total people: #{people.count}"
+
+female = people.select { |person| person.gender == 'f' }.count
+
+puts "Total females: #{female}"
+
+males = people.select { |person| person.gender == 'm' }.count
+
+puts "Total males: #{males}"
+
+# Count all the people who are older than you (or just pick an age).
+
+older_peeps = people.select { |person| person.age > 25 }.count
+
+puts "Total people older than 25: #{older_peeps}"
+
+# Count all the people who are younger than you (or just pick an age).
+
+younger_peeps = people.select { |person| person.age <= 25 }.count
+
+puts "Total people older than 25: #{younger_peeps}"
+
+# Count all the people whose first name and last name start with the
+# same letter.
+
+same_letter = people.select { |person| person.given_name.slice(0) == person.surname.slice(0) }.count
+
+puts "Total people with same first letter in name: #{same_letter}"
+
+# Calculate the average age of all the people.
+
+avg_age = people.map(&:age).reduce(:+) / people.length
+
+puts "Average age is: #{avg_age}"
